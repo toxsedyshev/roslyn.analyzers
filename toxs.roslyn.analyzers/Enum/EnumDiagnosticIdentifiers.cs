dcc6f9a591c2bd8f291b60fed3200c2c9468e559
Anton Sedyshev <me@toxs.ru>
2023-12-07 14:07:02 +0300

    DiagnosticIdentifiers changed

--- toxs.roslyn.analyzers/Enum/EnumDiagnosticIdentifiers.cs
+++ toxs.roslyn.analyzers/Enum/EnumDiagnosticIdentifiers.cs
@@ -2,10 +2,8 @@
 {
     public static class EnumDiagnosticIdentifiers
     {
-        private const string EnumPrefix = "ENUM";
+        private const string EnumPrefix = "TENUM";
 
         public const string SwitchOnEnumMustHandleAllCases = EnumPrefix + "0001";
-        public const string DefaultLabelShouldBeTheLast = EnumPrefix + "0002";
-        public const string MergeSwitchSectionsWithEquivalentContent = EnumPrefix + "0003";
     }
 }
